c37387046897fd9601a1bac83ec1370c3c2c78b0
Gibson Teo <gibsonjiacheng@gmail.com>
2021-07-02 23:24:04 +0800

    correcting reminder.py

--- src/reminder.py
+++ src/reminder.py
@@ -1,9 +1,11 @@
 
 def showReminders(bot, token):
     question = 'Please specify the division you want to make appointment.'
-    more_division_flex = json.load(open('../flex_templates/reminder.json', 'r', encoding='utf-8'))
+    more_division_flex = json.load(open('../flex_templates/contact.json', 'r', encoding='utf-8'))
     flex_template = FlexSendMessage(
         alt_text = 'Show the options to remind.',
         contents = more_division_flex
     )
     bot.reply_message(token, flex_template)
+    
+
