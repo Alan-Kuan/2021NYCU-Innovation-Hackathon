26c36ab9bd954c1caea63a3803936acb42e8f6e5
Alan-Kuan <kuanhaoan@gmail.com>
2021-07-02 21:39:13 +0800

    Change division selection function.

--- src/appointment.py
+++ src/appointment.py
@@ -10,20 +10,23 @@ import os
 divisionCode = json.load(open('../data/divisionCode.json', 'r', encoding='utf-8'))
 inv_divisionCode = {v: k for k, v in divisionCode.items()}
 
-def askForDivision(bot, token, division_data):
+def askForDivision(bot, token):
     question = 'Please specify the division you want to make appointment.'
     division_flex = json.load(open('../flex_templates/division.json', 'r', encoding='utf-8'))
     flex_template = FlexSendMessage(
-        alt_text = 'Ask for division.',
+        alt_text = 'Asking for division.',
         contents = division_flex
     )
     bot.reply_message(token, flex_template)
 
-def askForMoreDivision(bot, token, division_data):
+def askForMoreDivision(bot, token):
     question = 'Please specify the division you want to make appointment.'
     more_division_flex = json.load(open('../flex_templates/more_division.json', 'r', encoding='utf-8'))
     flex_template = FlexSendMessage(
-        alt_text = 'Ask for more division.',
+        alt_text = 'Asking for more division.',
         contents = more_division_flex
     )
     bot.reply_message(token, flex_template)
+
+def askForDate(bot, token, division_code):
+    pass
