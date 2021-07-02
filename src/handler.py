37158ee9927a7293f6b5b895915ac80f4e9e8b32
Alan-Kuan <kuanhaoan@gmail.com>
2021-07-03 04:37:31 +0800

    Fix some errors.

--- src/handler.py
+++ src/handler.py
@@ -137,16 +137,16 @@ def onPostback(event):
                 randCode = randCode + 1
             reminder.sendRandomCode(bot, event.reply_token, randCode)
         elif data == 'code':
-            reminder.requestRandCode(bot, token, user)
+            reminder.requestRandCode(bot, event.reply_token, user)
 
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
     msg = event.message.text
     user = event.source.user_id
-    cur_session = getSessionKey(user)
+    cur_session = db.getSessionKey(user)
     # Session Controls
     if 'rc_req' in cur_session:
-        rc_req = getSessionData(user, 'rc_req')
+        rc_req = db.getSessionData(user, 'rc_req')
         if rc_req[0] == True:
             reminder.comfirmRandCode(bot, token, user, msg)
     else:
