96f3ebebbe64004800046f56870c18a5ce90307f
Alan-Kuan <kuanhaoan@gmail.com>
2021-07-02 20:48:37 +0800

    Fix flex template path.

--- src/appointment.py
+++ src/appointment.py
@@ -38,18 +38,18 @@ inv_divisionCode = {v: k for k, v in divisionCode.items()}
 
 def askForDivision(bot, token, division_data):
     question = 'Please specify the division you want to make appointment.'
-    division_flex = json.load(open('./flex_templates/division.json', 'r', encoding='utf-8'))
+    division_flex = json.load(open('../flex_templates/division.json', 'r', encoding='utf-8'))
     flex_template = FlexSendMessage(
         alt_text = 'Ask for division.',
-        contents = division_flex 
+        contents = division_flex
     )
     bot.reply_message(token, flex_template)
 
 def askForMoreDivision(bot, token, division_data):
     question = 'Please specify the division you want to make appointment.'
-    more_division_flex = json.load(open('./flex_templates/more_division.json', 'r', encoding='utf-8'))
+    more_division_flex = json.load(open('../flex_templates/more_division.json', 'r', encoding='utf-8'))
     flex_template = FlexSendMessage(
         alt_text = 'Ask for more division.',
-        contents = more_division_flex 
+        contents = more_division_flex
     )
     bot.reply_message(token, flex_template)
